2291e0d4d1c29f93c27b0546c9dd530384018209
Josh Asadi <joshasadi@yahoo.com>
2022-10-28 00:19:31 -0500

    HAND

---
+++ Assets/AnimateHandOnInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty PinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = PinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripvalue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripvalue);
+    }
+}
